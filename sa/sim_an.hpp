d9fe0b798f4e6dbf285bd85128cca7b4de49c630
Andrew <chebanov.andrew@gmail.com>
2025-05-03 18:39:52 +0200

    changed cooling func

--- sa/sim_an.hpp
+++ sa/sim_an.hpp
@@ -1,4 +1,3 @@
-
 #ifndef SA
 #define SA
 
@@ -8,21 +7,22 @@
 #include "../Cities/city.hpp"
 
 
-std::pair<std::vector<City>, double> new_state(std::vector<City>& cities) {
+std::pair<std::vector<City>, double> new_state(std::vector<City>& cities) {    
     std::uniform_int_distribution<> dist(0, cities.size()-1);
 
     int pos1 = dist(gen);
     int pos2 = dist(gen);
 
-    if(pos1 == pos2) {
-        return std::pair{cities, total_cost(cities)};
-    }
-    else if (pos1 > pos2) {
+    while (pos1 == pos2) {
+        pos2 = dist(gen);
+    } 
+    
+    if (pos1 > pos2) {
         std::swap(pos1, pos2);
     }
 
     auto newCities = cities;
-    std::reverse(newCities.begin()+pos1, newCities.begin() + pos2);
+    std::reverse(newCities.begin() + pos1, newCities.begin() + pos2 + 1);
     
     return std::pair{newCities, total_cost(newCities)};
 
@@ -38,41 +38,43 @@ bool make_transition(long double P) {
     return false;
 }
 
-void sa_optimization(std::vector<City>& xs, double start_temp, double end_temp, double alpha, size_t steps) {
-    double currEnergy = total_cost(xs);
-    auto bestState = xs;
+void sa_optimization(std::vector<City>& cities, double start_temp, double end_temp, double alpha, size_t steps) {
+    std::shuffle(cities.begin(), cities.end(), gen);
+
+    double currEnergy = total_cost(cities);
+    auto bestState = cities;
     double bestEnergy = currEnergy;
 
     double T = start_temp;
     
     for(size_t i = 0; i < steps; ++i) {
         double newEnergy = 0;
-        auto tmp_state = new_state(xs);
+        auto tmp_state = new_state(cities);
         
         newEnergy = tmp_state.second;
 
         if(newEnergy < currEnergy) {
             currEnergy = newEnergy;
-            xs = tmp_state.first;
+            cities = tmp_state.first;
         }
         else {
             if(make_transition(exp(-((newEnergy-currEnergy)/T)))) {
-                xs = tmp_state.first;
+                cities = tmp_state.first;
                 currEnergy = newEnergy;
 
                 if(currEnergy < bestEnergy) {
                     bestEnergy = currEnergy;
-                    bestState = xs;
+                    bestState = cities;
                 }
             }
         }
 
-        T = start_temp / (1 + alpha * i); 
+        T *= alpha; 
 
         if(T <= end_temp) break; 
     }
 
-    xs = bestState;
+    cities = bestState;
 }
 
 
